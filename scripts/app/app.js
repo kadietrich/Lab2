2429a9f70893b5c0babe8838e1b7374d58b4b9a8
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-02-18 11:42:35 -0500

    Add background colors to page and tweet divs. Slow speed at which new tweets appear. Add fadeIn effect to tweets.

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,14 +1,15 @@
 
 $(document).ready(function(){
   var twitter = new ctwitter.CTwitter();
-  twitter.stream("statuses/filter", { lang:"en", track:["nvidia", "ati", "microsoft", ] }, function (stream){
+  twitter.stream("statuses/filter", { lang:"en", track:["nvidia", "ati", "microsoft"] }, function (stream){
     stream.on("data", function (tweet){
 		  $('<p class="tweet"><img src="'+tweet.profile_image_url+'" />'+tweet.text+'</p>')
 		    .hide()
 		    .prependTo('#tweets')
+        .fadeIn(500)
 		    .slideDown(50);
 		  $('#tweets p:gt(9)')
-		    .fadeOut(100, function() { $(this).remove() });
+		    .fadeOut(500, function() { $(this).remove() });
     });
   });
 });
